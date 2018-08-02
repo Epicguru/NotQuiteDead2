0cbaa7f43a8e2e2267dc9ef584a87821579b7d22
James Billy <james@epicguru.co.uk>
2018-08-02 23:59:01 +0200

    Lots of work gone into gun animation and system.

--- Assets/Scripts/Animation/GunAnimator.cs
+++ Assets/Scripts/Animation/GunAnimator.cs
@@ -2,19 +2,77 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class GunAnimator : StateMachineBehaviour
+public class GunAnimator : MonoBehaviour
 {
-    public string ShootState = "Shoot";
+    public Animator Anim;
+    public Gun Gun;
+
     public bool Stored;
     public bool Aiming;
 
-    public override void OnStateEnter(Animator animator, AnimatorStateInfo stateInfo, int layerIndex)
-    {
-        base.OnStateEnter(animator, stateInfo, layerIndex);
+    public bool Reloading;
+    public bool CheckingMag;
+    public bool Empty;
 
-        if (stateInfo.IsName(ShootState))
+    public const string SHOOT_NAME = "Shoot";
+    public const string STORED_NAME = "Stored";
+    public const string AIM_NAME = "Aim";
+    public const string EMPTY_NAME = "Empty";
+    public const string RELOAD_NAME = "Reload";
+    public const string CHECK_MAG_NAME = "Check Mag";
+
+    public const string SHOOT_CALLBACK = "Shoot";
+    public const string RELOAD_CALLBACK = "Reload";
+    public const string CHECK_MAG_CALLBACK = "Check Mag";
+
+    public static readonly int SHOOT_ID = Animator.StringToHash(SHOOT_NAME);
+    public static readonly int STORED_ID = Animator.StringToHash(STORED_NAME);
+    public static readonly int AIMING_ID = Animator.StringToHash(AIM_NAME);
+    public static readonly int EMPTY_ID = Animator.StringToHash(EMPTY_NAME);
+    public static readonly int RELOAD_ID = Animator.StringToHash(RELOAD_NAME);
+    public static readonly int CHECK_MAG_ID = Animator.StringToHash(CHECK_MAG_NAME);
+
+    public void AnimationCallback(AnimationEvent e)
+    {
+        if(e.stringParameter == SHOOT_CALLBACK)
         {
-            Debug.Log("Fired!");
+            Gun.CurrentMagCount--;
         }
+        if(e.stringParameter == RELOAD_CALLBACK)
+        {
+            Reloading = false;
+            Gun.CurrentMagCount = Gun.MagCapacity;
+        }
+        if(e.stringParameter == CHECK_MAG_CALLBACK)
+        {
+            CheckingMag = false;
+        }
+    }
+
+    public void Trigger(int id)
+    {
+        if (id == RELOAD_ID)
+        {
+            if (Reloading)
+                return;
+            Reloading = true;
+        }
+
+        if (id == CHECK_MAG_ID)
+        {
+            if (CheckingMag)
+                return;
+            CheckingMag = true;
+        }
+
+        Anim.SetTrigger(id);
+    }
+
+    public void Update()
+    {
+        Anim.SetBool(STORED_ID, Stored);
+        Anim.SetBool(AIMING_ID, Aiming);
+        Anim.SetBool(AIMING_ID, Aiming);
+        Anim.SetBool(EMPTY_ID, Empty);
     }
 }
---
+++ Assets/Scripts/Guns/Gun.cs
@@ -0,0 +1,119 @@
+﻿
+using UnityEngine;
+
+public class Gun : MonoBehaviour
+{
+    public GunAnimator Anim;
+
+    public int MagCapacity = 31;
+    public int CurrentMagCount = 31;
+
+    public float RPM = 500f;
+    public bool Shoot;
+    public bool Reload;
+    public bool CheckMag;
+    public bool Aim;
+
+    public KeyCode ShootKey = KeyCode.Mouse0;
+    public KeyCode AimKey = KeyCode.Mouse1;
+    public KeyCode ReloadKey = KeyCode.R;
+    public KeyCode CheckMagKey = KeyCode.F;
+
+    private float gunTimer = 100f;
+
+    public void Update()
+    {
+        // Collect raw input. The input must then be validated.
+        CollectInput();
+
+        if (Reload && !CanReload())
+            Reload = false;
+        if (Aim && !CanAim())
+            Aim = false;
+        if (CheckMag && !CanCheckMag())
+            CheckMag = false;
+        if (Shoot && !CanShoot())
+            Shoot = false;
+
+        if (Reload || Anim.Reloading)
+            Anim.Aiming = false;
+
+        if (CheckMag || Anim.CheckingMag)
+            Anim.Aiming = false;
+
+        // Update shooting timer...
+        gunTimer += Time.deltaTime;
+
+        if(gunTimer >= GetShootingInterval())
+        {
+            if (Shoot)
+            {
+                gunTimer = 0f;
+
+                ShootGun();
+            }
+        }
+
+        Anim.Empty = CurrentMagCount == 0;
+
+        if (Reload)
+        {
+            Anim.Trigger(GunAnimator.RELOAD_ID);
+            Reload = false;
+        }
+
+        if (CheckMag)
+        {
+            Anim.Trigger(GunAnimator.CHECK_MAG_ID);
+            CheckMag = false;
+        }
+        
+        Anim.Aiming = Aim;        
+    }
+
+    public void OnGUI()
+    {
+        GUILayout.Label("Bullets: {0}".Form(CurrentMagCount));
+    }
+
+    public void CollectInput()
+    {
+        Shoot = Input.GetKey(ShootKey);
+        Reload = Input.GetKeyDown(ReloadKey);
+        CheckMag = Input.GetKeyDown(CheckMagKey);
+        Aim = Input.GetKey(AimKey);
+    }
+
+    public bool CanAim()
+    {
+        return !Anim.Stored && !Anim.Reloading && !Anim.CheckingMag;
+    }
+
+    public bool CanReload()
+    {
+        return !Anim.Stored && !Anim.Reloading && !Anim.CheckingMag;
+    }
+
+    public bool CanCheckMag()
+    {
+        return !Anim.Stored && !Anim.Reloading && !Anim.CheckingMag;
+    }
+
+    public bool CanShoot()
+    {
+        return !Anim.Stored && Anim.Aiming && this.CurrentMagCount > 0 && !Anim.Reloading && !Anim.CheckingMag;
+    }
+
+    public void ShootGun()
+    {
+        Anim.Trigger(GunAnimator.SHOOT_ID);
+    }
+
+    public float GetShootingInterval()
+    {
+        const float SECONDS_PER_MINUTE = 60f;
+        float rps = RPM / SECONDS_PER_MINUTE;
+
+        return 1f / rps;
+    }
+}
