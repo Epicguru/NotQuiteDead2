a1d09ef12b757b721b5f9d690e06ab7cc0d4697c
James Billy <james@epicguru.co.uk>
2018-08-19 19:33:03 +0200

    Writing command execution code.

---
+++ Assets/Scripts/Commands/CommandExec.cs
@@ -0,0 +1,29 @@
+﻿
+using System;
+using System.Linq;
+using System.Reflection;
+using UnityEngine;
+
+public static class CommandExec
+{
+    private static void GetCommandAttributes()
+    {
+        Assembly a = typeof(CommandExec).Assembly;
+        Console.WriteLine("Searching for custom commands and variables in assembly '{0}'".Form(a));
+
+        // Partition on the type list initially.
+        var found = from t in a.GetTypes().AsParallel()
+            let attributes = t.GetCustomAttributes(typeof(DebugCommandAttribute), true)
+            where attributes != null && attributes.Length > 0
+            select new { Type = t, Methods = from x in t.GetMethods() where x.IsDefined(typeof(DebugCommandAttribute), true), Attributes = attributes.Cast<DebugCommandAttribute>() };
+
+        foreach (var attr in found)
+        {
+            var type = attr.Type;
+            var methods = attr.Methods;
+            var attributes = attr.Attributes;
+
+            Debug.Log("Type '{0}' has {1} attributes with {2} methods:\n".Form(type.FullName, attributes.Count(), methods.Count()));
+        }
+    }
+}
---
+++ Assets/Scripts/Commands/DebugCommandAttribute.cs
@@ -0,0 +1,13 @@
+﻿
+using System;
+
+[AttributeUsage(AttributeTargets.Method, AllowMultiple = false, Inherited = true)]
+public class DebugCommandAttribute : Attribute
+{
+    public string Description { get; private set; }
+
+    public DebugCommandAttribute(string description)
+    {
+        Description = description;
+    }
+}
