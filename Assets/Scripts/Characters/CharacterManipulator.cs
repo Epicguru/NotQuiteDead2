71dc79306d25075a3791e31866c3436b518f855a
James Billy <james@epicguru.co.uk>
2018-09-21 10:21:24 +0200

    Added some notes to self about character architecture.

--- Assets/Scripts/Characters/CharacterManipulator.cs
+++ Assets/Scripts/Characters/CharacterManipulator.cs
@@ -5,6 +5,18 @@ using UnityEngine.Networking;
 
 public class CharacterManipulator : NetworkBehaviour
 {
+    // Essentially owns a character, which they can then control. Characters can actually be controlled and changed without
+    // being a character manipulator, but this is the formal way of doing things.
+
+    public bool IsBot { get; set; }
+    public bool IsPlayer
+    {
+        get
+        {
+            return Player != null;
+        }
+    }
+
     public Character Target
     {
         get
@@ -43,4 +55,27 @@ public class CharacterManipulator : NetworkBehaviour
     }
     private Player _player;
 
+    public Vector2 MovementDirection
+    {
+        get
+        {
+            return Target.Movement.NormalizedInputDirection;
+        }
+        set
+        {
+            Target.Movement.NormalizedInputDirection = value;
+        }
+    }
+
+    public float MovementSpeed
+    {
+        get
+        {
+            return Target.Movement.CurrentSpeed;
+        }
+        set
+        {
+            Target.Movement.CurrentSpeed = value;
+        }
+    }
 }
